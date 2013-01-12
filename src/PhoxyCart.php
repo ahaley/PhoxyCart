e2d9c99964872ed91145ed841b70063c3065582d
Antonio Haley <antonio.haley@gmail.com>
2013-01-12 21:24:22 +0100

    Existance check for config file
    An error message is given when the configuratio file has not
    been generating by the build script.

--- src/PhoxyCart.php
+++ src/PhoxyCart.php
@@ -1,6 +1,10 @@
 <?php
 
-require_once __DIR__ . '/../config/config.php';
+define('CONFIG_FILE', __DIR__ . '/../config/config.php');
+if (!file_exists(CONFIG_FILE)) {
+    die('Configuration file needs to be built' . PHP_EOL);
+}
+require_once CONFIG_FILE;
 require_once 'SplClassLoader.php';
 
 $loader = new SplClassLoader('PhoxyCart', __DIR__);
